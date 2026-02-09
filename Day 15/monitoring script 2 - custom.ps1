627c8779d8729f62a12ee8a5ac0aacc2a98a6420
jeetu <jitendrastomar5593@gmail.com>
2026-02-09 18:22:44 +0530

    adding files

---
+++ Day 15/monitoring script 2 - custom.ps1
@@ -0,0 +1,10 @@
+﻿cls
+$prc = Get-NetTCPConnection | `
+        Where-Object {$_.RemotePort -eq 443} | `
+        Select-Object LocalAddress, RemoteAddress, OwningProcess 
+foreach($p in $prc){
+    if($p.owningprocess -ne 0){
+        $pron = (Get-Process -Id $p.OwningProcess).ProcessName
+    }
+Write-host $p.LocalAddress " ---> " $p.RemoteAddress  " --->  "  $pron
+}
---
+++ Day 15/monitoring script.ps1
@@ -0,0 +1,24 @@
+﻿# failed login/logout 
+Get-WinEvent -FilterHashtable @{LogName='Security'; Id=4625} | ft -AutoSize -Wrap
+
+# to view active web connections.
+Get-NetTCPConnection    # displays everything
+
+# connection to HTTP port
+Get-NetTCPConnection | Where-Object {$_.RemotePort -eq 80}   
+
+# connection to HTTPS port
+Get-NetTCPConnection | Where-Object {$_.RemotePort -eq 443} 
+
+# connection to HTTPS or HTTP port 
+Get-NetTCPConnection | Where-Object {$_.RemotePort -eq 80 -or $_.RemotePort -eq 443 }
+
+# mapping the connection to the process
+Get-NetTCPConnection | `
+Where-Object {$_.RemotePort -eq 443} | `
+Select-Object LocalAddress, RemoteAddress, OwningProcess
+
+# resolving the process
+Get-Process -Id 2644
+Get-Process -Id 11048
+Get-Process -Id 10712
